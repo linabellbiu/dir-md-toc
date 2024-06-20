1d32a0bf412ef1df398fdcb76b72f99886fb53dc
linabellbiu <coke10086@gmail.com>
2024-06-20 19:24:58 +0800

    create

---
+++ dir_md_toc.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// 读取忽略列表文件
+func readIgnoreFile(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var ignoreList []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line != "" && !strings.HasPrefix(line, "#") { // 忽略空行和注释行
+			ignoreList = append(ignoreList, line)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return ignoreList, nil
+}
+
+// 判断文件或文件夹是否在忽略列表中
+func isIgnored(name string, ignoreList []string) bool {
+	for _, ignore := range ignoreList {
+		if name == ignore || strings.HasPrefix(name, ignore) {
+			return true
+		}
+	}
+	return false
+}
+
+// 列出文件并生成markdown
+func listFiles(path string, level int, file *os.File, ignoreList []string) {
+	files, err := os.ReadDir(path)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, fileInfo := range files {
+		if isIgnored(fileInfo.Name(), ignoreList) {
+			continue
+		}
+		if fileInfo.IsDir() && !strings.HasPrefix(fileInfo.Name(), ".") {
+			_, _ = fmt.Fprintln(file, strings.Repeat("  ", level)+"- ["+fileInfo.Name()+"]("+path+"/"+fileInfo.Name()+")")
+			listFiles(path+"/"+fileInfo.Name(), level+1, file, ignoreList)
+		} else if !fileInfo.IsDir() && !strings.HasPrefix(fileInfo.Name(), ".") {
+			_, _ = fmt.Fprintln(file, strings.Repeat("  ", level)+"- ["+fileInfo.Name()+"]("+path+"/"+fileInfo.Name()+")")
+		}
+	}
+}
+
+func main() {
+	path := "./"
+	ignoreFilePath := "./.ignore"
+
+	// 读取忽略列表
+	ignoreList, err := readIgnoreFile(ignoreFilePath)
+	if err != nil {
+		fmt.Println("Error reading ignore file:", err)
+		return
+	}
+
+	f, err := os.Create("README.md")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	_, _ = fmt.Fprintf(f, "# 目录\n\n")
+	listFiles(path, 1, f, ignoreList)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/linabellbiu/dir-md-toc
+
+go 1.21.8
